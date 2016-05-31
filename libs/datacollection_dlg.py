b1e5ea4394f3e02371550712648c72084d00c353
Mattias Måhl <mattias@kejsarsten.com>
2016-05-31 10:55:14 +0200

    dokumentation strings in Collectdata_dlg

--- libs/datacollection_dlg.py
+++ libs/datacollection_dlg.py
@@ -15,10 +15,28 @@ else:
 
 
 class regexp_strings(object):
+    """object to store the regex-strings to check for valid IP-adresses!"""
     ipv4 = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
     short_mask = "^(?:(3[0-2]|[01]?[0-2][0-9]?|[1]?[0-9]?))$"
 
 class Collectdata(object):
+    """
+    Collect data dialog to add data to the main-frame window.
+    Use:
+       dlg = Collectdata(parent=root, title="your title", edit=True, gfxpath="gfx/)
+       options:
+           parent   : parent window if there is one.
+           title    : custom title else defaults to "Add target"
+           edit     : True or False, defaults to None
+           gfxpath  : Where the graphics is located.
+           
+    Returns four values
+    Valid : (True or False)
+    ip    : Ip-adress, single, network och the first in a range.
+    ip2   : second IP-adress in a range
+    mask  : if entered as input.
+    """
+    
     Cancel=False
     def __init__(self, parent=None, title="Add target", edit=None, gfxpath="gfx/"):
         self.gfxpath=gfxpath
@@ -65,6 +83,12 @@ class Collectdata(object):
         self.frame.rowconfigure(1, weight=1)
         
     def show(self):
+        """
+        Shows the dialog and returns the values based on input.
+        use:
+            dlg = Collectdata()
+            valid, ip, ip2, mask = dlg.show()
+        """
         self.rootdlg.wait_window()
         if self.Cancel:
             return None, None, None, None
@@ -72,7 +96,9 @@ class Collectdata(object):
             return self.returnvalues
 
     def Collectiondata_OK(self, *event):
-        
+        """
+        Buttonactivated function.
+        """
         data = self.validate(self.data.get(), returnvalues=True)
         if not data[0]:
             messagebox.showerror("Input error!", "Your information is not a valid input.\nPlease check your input.", parent=self.rootdlg)
@@ -81,22 +107,32 @@ class Collectdata(object):
             self.rootdlg.destroy()
 
     def Collectiondata_Cancel(self, *event):
+        """ button activated function"""
         self.Cancel=True
         self.rootdlg.destroy()
 
     def onvalidate(self, d,i,P,s,S,v,V,W):
+        """ private validation function to check if it is a valid input i entrybox"""
         newcolor = 'IndianRed1' if not self.validate(P) else self.default_color
         self.rootdlg.nametowidget(W).configure(background=newcolor)
         return True
 
     @staticmethod
     def isIP(ip):
+        """
+        staticmethod that returns True or False on a textstring.
+        checks if it is a valid ip address.
+        """
         if re.search(regexp_strings.ipv4, ip):
             return True
         else:
             return False
     @staticmethod
     def isMASK(mask):
+        """
+        staticmethod that returns True or False on a textstring.
+        checks if it is a valid mask.
+        """
         if re.search(regexp_strings.ipv4, mask) or (len(mask) < 3 and re.search(regexp_strings.short_mask, mask)):
             return True
         else:
@@ -104,7 +140,11 @@ class Collectdata(object):
 
     @staticmethod
     def validate(ip, returnvalues=False):
-        #:TODO: check the mask if there is one, and add support for ranged input. e.g. 10.10.10.10-10.10.10.30
+        """
+        staticmethod that returns True/False or the returnvalus for the dialog on a textstring.
+        checks if it is a valid input for setting background on Entry or return values for the dialog.
+        """
+        
         valid=True
         sep = "/"
         net = net2 = mask = None
